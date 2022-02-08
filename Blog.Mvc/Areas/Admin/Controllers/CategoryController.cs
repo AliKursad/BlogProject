4044216a01c4e52cd7ed943b92ead3b44273593e
Ali Kürşad Çorapsız <KursadCor@outlook.com>
2022-02-08 21:52:08 +0300

    Ajax post

--- Blog.Mvc/Areas/Admin/Controllers/CategoryController.cs
+++ Blog.Mvc/Areas/Admin/Controllers/CategoryController.cs
@@ -1,6 +1,10 @@
-﻿using Blog.Services.Abstract;
+﻿using Blog.Entities.Dtos;
+using Blog.Mvc.Areas.Admin.Models;
+using Blog.Services.Abstract;
+using Blog.Shared.Utilities.Extensions;
 using Blog.Shared.Utilities.Results.ComplexTypes;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace Blog.Mvc.Areas.Admin.Controllers
@@ -22,5 +26,34 @@ namespace Blog.Mvc.Areas.Admin.Controllers
             return View(result.Data);
             
         }
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return PartialView("_CategoryAddPartial");
+        }
+        [HttpPost]
+        public async Task<IActionResult> Add(CategoryAddDto categoryAddDto)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _categoryService.Add(categoryAddDto,"Ali Kursad");
+                if (result.ResultStatus == ResultStatus.Success)
+                {
+                    var categoryAddAjaxModel = JsonSerializer.Serialize(new CategoryAddAjaxViewModel
+                    {
+                        CategoryDto = result.Data,
+                        CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto)
+                    });
+                    return Json(categoryAddAjaxModel);
+                }
+            }
+
+            var categoryAddAjaxErrorModel = JsonSerializer.Serialize(new CategoryAddAjaxViewModel
+            {
+                CategoryAddPartial = await this.RenderViewToStringAsync("_CategoryAddPartial", categoryAddDto)
+            });
+            return Json(categoryAddAjaxErrorModel);
+        }
+
     }
 }
--- Blog.Shared/Entities/Abstract/EntityBase.cs
+++ Blog.Shared/Entities/Abstract/EntityBase.cs
@@ -9,7 +9,7 @@ namespace Blog.Shared.Entities.Abstract
     public abstract class EntityBase
     {
         public virtual int Id { get; set; }
-        public virtual DateTime CreatedDate { get; set; } = DateTime.Now; // override CreatedDate = new DateTime(2022/01/01)
+        public virtual DateTime CreatedDate { get; set; } = DateTime.Now;
         public virtual DateTime ModifiedDate { get; set; } = DateTime.Now;
         public virtual bool IsDeleted { get; set; } = false;
         public virtual bool IsActive { get; set; } = true;
