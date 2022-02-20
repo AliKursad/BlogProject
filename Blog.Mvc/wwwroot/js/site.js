722304e64891c7933ee3292f445b7d5ed3e1970d
Ali Kürşad Çorapsız <KursadCor@outlook.com>
2022-02-20 18:18:54 +0300

    UI changes

--- Blog.Mvc/wwwroot/js/site.js
+++ Blog.Mvc/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿
+﻿function convertFirstLetterToUpperCase(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+function convertToShortDate(dateString) {
+    const shortDate = new Date(dateString).toLocaleDateString('en-US');
+    return shortDate;
+}
+
